8057ab6f53e13f28036c14b9c8b8abc9300452fe
girikumar <girikumar1116@gmail.com>
2023-05-01 10:01:10 +0530

    secondlargest

---
+++ SecondLargest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class SecondLargest
+    {
+        /*static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            int max, v;
+            Console.WriteLine("Enter {0} elements in the array", size);
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            max = arr[0];
+            v = arr[0];
+
+            for (int i = 1; i < size; i++)
+            {
+
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            int temp = max;
+            for (int i = 1; i < size; i++)
+            {
+
+                if ((arr[i] > v) && (arr[i] != temp))
+                {
+                    v = arr[i];
+                }
+            }
+
+            Console.Write("Maximum element = {0}\n", v);
+            Console.ReadLine();
+        }*/
+    }
+}
