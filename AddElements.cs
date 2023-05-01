04513f4b9cf9f7abf8aae8b251f7530ffa642e76
girikumar <girikumar1116@gmail.com>
2023-05-01 09:17:28 +0530

    [giri]Addelements

---
+++ AddElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class AddElements
+    {
+        
+       /* public static void Main(string[] args)
+        {
+            Console.WriteLine("enter a size of array");
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (int j in arr)
+            {
+                sum += j;
+            }
+            Console.WriteLine("sum of the arr is" + sum);
+        }*/
+    }
+}
