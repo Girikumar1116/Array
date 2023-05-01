e15a362e9d2e248bd481fc287fd4ccb4add13415
girikumar <girikumar1116@gmail.com>
2023-05-01 10:02:43 +0530

    unique value

---
+++ Unique.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Unique
+    {
+       /* public static void Main(string[] args)
+        {
+            Console.WriteLine("enter a arr size");
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < len; i++)
+            {
+                bool flag = false;
+                for (int j = i + 1; i < len-1; i++)
+                {
+                    if (j == len - 1)
+                        break;
+                    if (arr[i] == arr[j])
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false)
+                    Console.WriteLine(arr[i]);
+            }
+        }*/
+    }
+}
