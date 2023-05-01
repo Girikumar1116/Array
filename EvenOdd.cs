499bf93d22dd5f777b5190e8a5aee26dbb086051
girikumar <girikumar1116@gmail.com>
2023-05-01 09:46:01 +0530

    evenoddelements

---
+++ EvenOdd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class EvenOdd
+    {
+       /* public static void Main()
+        {
+            Console.WriteLine("enter size of arr");
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int even = 0;
+            int odd = 0;
+            foreach (int i in arr)
+            {
+                if (i % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                { odd++; }
+               
+            }
+            Console.WriteLine("even elements"+ even);
+            Console.Write("odd elements "+ odd);
+        }*/
+    }
+}
